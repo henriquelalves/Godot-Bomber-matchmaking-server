ce60c9e9a65bd479b543909ae5d08345b2220a60
Henrique L Alves <henriquelalves@gmail.com>
2018-01-26 13:43:53 -0200

    Testing tcp/udp server

--- server/server.js
+++ server/server.js
@@ -1,115 +1,122 @@
 const net = require('net');
 const http = require('http');
+const dgram = require('dgram');
 
 var player_count = 0;
-var host = '0.0.0.0'; // Check for every incoming IP.
-var port = 3456; // Port we want to spin up our server on.
+const HOST = '0.0.0.0'; // Check for every incoming IP.
+const TCP_PORT = 3457; // Port we want to spin up our server on.
+const UDP_PORT = 3456
 
-var player_queue = [];
+var udpserver = dgram.createSocket('udp4');
+var tcpserver;
 
-// Read chunks out of buffer and queue player on the end
-function read_chunks(socket, buffer) {
-	// chunks are: 0. name-string size; 1. name-string; 2. ip-string size; 3. ip-string
-	var current_byte = 0;
+var players_connecting = {}
+var players_connected = {}
+var players_hosting = [];
 
-	console.log(buffer);
-	console.log(buffer.length);
+function refresh(){
 
-	while (buffer.length > current_byte) {
-		console.log(current_byte, " ", socket.current_chunk);
-		if (socket.current_chunk === 0) {
-			socket.player_name_size = buffer.readUIntLE(current_byte, 4);
-			current_byte += 4;
-		} else if (socket.current_chunk === 1) {
-			socket.player_name = buffer.toString('utf8', current_byte, current_byte + socket.player_name_size);
-			current_byte += socket.player_name_size;
-		} else if (socket.current_chunk === 2) {
-			socket.player_ip_size = buffer.readUIntLE(current_byte, 4);
-			current_byte += 4;
-		} else if (socket.current_chunk === 3) {
-			console.log(current_byte, socket.player_ip_size);
-			socket.player_ip = buffer.toString('utf8', current_byte, current_byte + socket.player_ip_size);
-			current_byte += socket.player_ip_size;
+}
+
+function host(){
+
+}
+
+function add_player(pid, psocket) {
+	psocket.private_ip = pid.split(':')[1];
+
+	players_connecting[pid] = {
+		remote_ip: pid.split(':')[0],
+		private_ip: pid.split(':')[1],
+		remote_port: false,
+		name: false,
+		socket: psocket
+	}
+}
+
+function set_player_name(pid, name){
+	players_connecting[pid].name = name;
+}
+
+function check_player_connection(pid, psocket) {
+	var player = players_connecting[pid];
+	if (player && player.name) {
+		console.log("UDP > Registering new player from " + pid)
+		players_connected[pid] = {
+			remote_ip: player.remote_ip,
+			private_ip: player.private_ip,
+			remote_port: psocket.remotePort,
+			name: player.name,
+			socket: player.socket
 		}
-		socket.current_chunk++;
-	}
 
-	if (socket.current_chunk === 4) {
-		player_queue.push([socket, socket.player_name, socket.player_ip]);
-		console.log("New player on queue!");
-		console.log('Player remoteAddress: ', socket.remoteAddress);
-		console.log('Player remotePort: ', socket.remotePort);
-		console.log('Player privateAddress: ', socket.player_ip);
-		console.log('Player privatePort: ', 3457);
-		console.log(player_queue.length);
 	}
-
-	// Check if can arrange a match
-	check_match();
+	players_connecting[pid] = false;
 }
 
-// Check if match can be arranged
-function check_match() {
-	if (player_queue.length >= 2) {
-		console.log("Match found!");
+// ================== TCP ====================
 
-		send_var(1, player_queue[1][0]); // Master
-		send_var(player_queue[0][1], player_queue[1][0]); // Send name
-		send_var(player_queue[0][0].remoteAddress, player_queue[1][0]); // Send remote IP
-		send_var(player_queue[0][0].remotePort, player_queue[1][0]); // Send remote PORT
-		send_var(player_queue[0][2], player_queue[1][0]); // Send private IP
+// Read chunks out of buffer
+function read_chunks(socket, buffer) {
+	var str = buffer.toString('utf8');
+	var type = str.substr(0, 3);
+	var pid = socket.remoteAddress + ':' + socket.private_ip;
+	console.log("TCP > " + pid + " " + str);
 
-		send_var(0, player_queue[0][0]); // Slave
-		send_var(player_queue[1][1], player_queue[0][0]); // Send name
-		send_var(player_queue[1][0].remoteAddress, player_queue[0][0]); // Send remote IP
-		send_var(player_queue[1][0].remotePort, player_queue[0][0]); // Send remote PORT
-		send_var(player_queue[1][2], player_queue[0][0]); // Send private IP
+	if (type === "reg"){
+		add_player(socket.remoteAddress + ":" + str.substr(3), socket);
+	}
+	else if (type === "nam"){
+		set_player_name(pid, str.substr(3));
+		send_str("hey", socket);
+	} else if (type === "con"){
 
-		player_queue.splice(0, 2);
 	}
 }
 
-function send_var(data, socket) {
-	var response = null;
-
-	if (typeof (data) === 'string') {
-		response = new Buffer(4 + Buffer.byteLength(data));
-		response.writeUIntLE(data.length, 0, 4); // Size of string
-		response.write(data, 4); // String
-	} else if (typeof (data) === 'number') {
-		response = new Buffer(4);
-		response.writeUIntLE(data, 0, 4);
-	}
-
+function send_str(data, socket) {
+	var response = new Buffer(Buffer.byteLength(data));
+	console.log("TCP > Sending to " + socket.remoteAddress +  ":" + socket.private_ip + " " + data);
+	response.write(data, 0); // String
 	socket.write(response);
 }
 
 // When a player connects to the server
-var server = net.createServer(function (socket) {
-
-	player_count++;
-	console.log('--- Player number %d connected ---', player_count)
-	console.log('Socket address: ', socket.address());
-
-	socket.current_chunk = 0;
-
+tcpserver = net.createServer(function (socket) {
 	// Server socket received data
 	socket.on('data', function (buffer) {
 		read_chunks(this, buffer);
-	})
+	});
 
 	// Player ended connection
-	socket.on('end', function () {
+	// socket.on('end', function () {
 
-		console.log('Client disconnected')
-		player_count--;
-	})
+	// 	console.log('Client disconnected')
+	// 	player_count--;
+	// });
 })
 
-server.listen(port, host, function () {
-	console.log("Hey, listen!");
+server.listen(TCP_PORT, HOST, function () {
+	console.log("TCP, listen!");
 })
 
+// ================== UDP =================
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP, listen!');
+});
+
+server.on('message', function (message, remote) {
+    var string = message.toString('utf8');
+    // console.log(remote.address + ':' + remote.port + ' - ' + string);
+    var player_id = remote.address + ":" + string;
+	check_player_connection(player_id);
+});
+
+server.bind(UDP_PORT, HOST);
+
+// ========================================
 // Lazy HTTP response.
 var httpserver = http.createServer(function (request, response) {
 	response.writeHead(200, { "Content-Type": "text/html" });
